a924c3361f2e78e30203e02a2db5e2936acacaa6
Mortonian <mortonian@server.fake>
2013-10-05 20:36:47 -0400

    more thoughts

--- src/test/java/com/opower/connectionpool/TestConnectionPool.java
+++ src/test/java/com/opower/connectionpool/TestConnectionPool.java
@@ -3,9 +3,12 @@ package com.opower.connectionpool;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.junit.Test;
+
+import com.opower.connectionpool.ConnectionPool;
 
 public class TestConnectionPool {
     
@@ -16,13 +19,38 @@ public class TestConnectionPool {
     @Test
     public void testStuff() {        
         try {
+            String url = "jdbc:postgresql://"+HOSTNAME+"/"+DATABASE+"?"+ "user="+USERNAME;
+
             Class.forName("org.postgresql.Driver");
-            Connection connect = DriverManager.getConnection("jdbc:postgresql://"+HOSTNAME+"/"+DATABASE+"?"+ "user="+USERNAME);
+            DumbConnectionPool innerClass = new DumbConnectionPool();
+            innerClass.setUrl(url);
+            Connection connect = innerClass.getConnection();
             Statement statement = connect.createStatement();
             ResultSet resultSet = statement.executeQuery("select count(*) from table");
             resultSet.next();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    
+    public static class DumbConnectionPool implements ConnectionPool {
+        
+        private String _url;
+        
+        public void setUrl(String url) {
+            _url = url;
+        }
+
+        @Override
+        public Connection getConnection() throws SQLException {
+    
+            Connection connect = DriverManager.getConnection(_url);
+            return connect;
+        }
+
+        @Override
+        public void releaseConnection(Connection connection) throws SQLException {
+            //  TODO
         }
     }
 }
