266be812513df68cb0692dac2ec2bcc9f9266f2a
Mortonian <mortonian@server.fake>
2013-10-07 01:02:24 -0400

    bah!  My terrible spelling

--- src/test/java/com/opower/connectionpool/TestSimpleConnectionPool.java
+++ src/test/java/com/opower/connectionpool/TestSimpleConnectionPool.java
@@ -22,19 +22,19 @@ public class TestSimpleConnectionPool {
         ConnectionDescriptor mockConnectionDescriptor = EasyMock.createMock(ConnectionDescriptor.class);
         Connection mockConnection = EasyMock.createMock(Connection.class);
         ConnectionCreator mockConnectionCreator = new MockConnectionCreator(mockConnection);
-        SimpleConnectionPool connecionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 0);
+        SimpleConnectionPool connectionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 0);
         
         Connection connection;
         try {
-            Assert.assertEquals("Number of connections handed out should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
-            connection = connecionPool.getConnection();
+            Assert.assertEquals("Number of connections handed out should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
+            connection = connectionPool.getConnection();
             Assert.assertNull("Connection returned from a zero-sized pool should be null", connection);
-            Assert.assertEquals("Number of connections handed out after get connection should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
-            connecionPool.releaseConnection(connection);
-            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after release connection should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
+            Assert.assertEquals("Number of connections handed out after get connection should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
+            connectionPool.releaseConnection(connection);
+            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after release connection should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
         } catch (SQLException e) {
             _log.error("Error testing zero-sized connection pool", e);
         }  
@@ -45,23 +45,23 @@ public class TestSimpleConnectionPool {
         ConnectionDescriptor mockConnectionDescriptor = EasyMock.createMock(ConnectionDescriptor.class);
         Connection mockConnection = EasyMock.createMock(Connection.class);
         ConnectionCreator mockConnectionCreator = new MockConnectionCreator(mockConnection);
-        SimpleConnectionPool connecionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 1);
+        SimpleConnectionPool connectionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 1);
         
         Connection connection;
         try {
-            Assert.assertEquals("Number of connections handed out should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
-            connection = connecionPool.getConnection();
+            Assert.assertEquals("Number of connections handed out should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
+            connection = connectionPool.getConnection();
             Assert.assertNotNull("Connection returned from a zero-sized pool should not be null", connection);
-            Assert.assertEquals("Number of connections handed out after get connection should be 1", 1, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
-            connecionPool.releaseConnection(connection);
-            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connecionPool.getNumberOfConnectionsAvailable());
-            connecionPool.releaseConnection(connection);
-            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connecionPool.getNumberOfConnectionsAvailable());
-            Connection connection2 = connecionPool.getConnection();
+            Assert.assertEquals("Number of connections handed out after get connection should be 1", 1, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
+            connectionPool.releaseConnection(connection);
+            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connectionPool.getNumberOfConnectionsAvailable());
+            connectionPool.releaseConnection(connection);
+            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connectionPool.getNumberOfConnectionsAvailable());
+            Connection connection2 = connectionPool.getConnection();
             String uuid1 = ((PooledConnectionInfo)connection).getConnectionUuid();
             String uuid2 = ((PooledConnectionInfo)connection2).getConnectionUuid();
             Assert.assertEquals("Should be the same connection, got UUIDs: "+uuid1+", "+uuid2, uuid1, uuid2);
@@ -77,7 +77,7 @@ public class TestSimpleConnectionPool {
         Connection mockConnection = EasyMock.createMock(Connection.class);
         Statement mockStatement = EasyMock.createMock(Statement.class);
         ConnectionCreator mockConnectionCreator = new MockConnectionCreator(mockConnection);
-        SimpleConnectionPool connecionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 1);
+        SimpleConnectionPool connectionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 1);
 
         Connection connection;
         try {
@@ -85,12 +85,12 @@ public class TestSimpleConnectionPool {
             EasyMock.expect(mockConnection.createStatement()).andReturn(mockStatement);
             EasyMock.replay(mockConnection);
             
-            connection = connecionPool.getConnection();
+            connection = connectionPool.getConnection();
             Statement statement = connection.createStatement();
             Assert.assertTrue("Connection should still be valid", ((PooledConnectionInfo)connection).isLeaseValid());
             Assert.assertNotNull("Valid connections should return non-null statements", statement);
             
-            connecionPool.releaseConnection(connection);
+            connectionPool.releaseConnection(connection);
             Assert.assertFalse("Connection should no longer be valid", ((PooledConnectionInfo)connection).isLeaseValid());
             try {
                 connection.createStatement();
@@ -108,40 +108,40 @@ public class TestSimpleConnectionPool {
         ConnectionDescriptor mockConnectionDescriptor = EasyMock.createMock(ConnectionDescriptor.class);
         Connection mockConnection = EasyMock.createMock(Connection.class);
         ConnectionCreator mockConnectionCreator = new MockConnectionCreator(mockConnection);
-        SimpleConnectionPool connecionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 2);
+        SimpleConnectionPool connectionPool = new SimpleConnectionPool(mockConnectionDescriptor, mockConnectionCreator, 2);
         
         Connection connection;
         try {
-            Assert.assertEquals("Number of connections handed out should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
-            connection = connecionPool.getConnection();
+            Assert.assertEquals("Number of connections handed out should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
+            connection = connectionPool.getConnection();
             Assert.assertNotNull("Connection returned from a zero-sized pool should not be null", connection);
-            Assert.assertEquals("Number of connections handed out after get connection should be 1", 1, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
-            connecionPool.releaseConnection(connection);
-            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connecionPool.getNumberOfConnectionsAvailable());
+            Assert.assertEquals("Number of connections handed out after get connection should be 1", 1, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
+            connectionPool.releaseConnection(connection);
+            Assert.assertEquals("Number of connections handed out after release connection should be 0", 0, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connectionPool.getNumberOfConnectionsAvailable());
             
-            Connection connection2 = connecionPool.getConnection();
+            Connection connection2 = connectionPool.getConnection();
             Assert.assertNotNull("Connection returned from a zero-sized pool should not be null", connection2);
-            Assert.assertEquals("Number of connections handed out after get connection should be 1", 1, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
+            Assert.assertEquals("Number of connections handed out after get connection should be 1", 1, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
             
             String uuid1 = ((PooledConnectionInfo)connection).getConnectionUuid();
             String uuid2 = ((PooledConnectionInfo)connection2).getConnectionUuid();
             Assert.assertEquals("Should be the same connection, got UUIDs: "+uuid1+", "+uuid2, uuid1, uuid2);
             
-            Connection connection3 = connecionPool.getConnection();
+            Connection connection3 = connectionPool.getConnection();
             Assert.assertNotNull("Connection returned from a zero-sized pool should not be null", connection3);
-            Assert.assertEquals("Number of connections handed out after get connection should be 1", 2, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connecionPool.getNumberOfConnectionsAvailable());
+            Assert.assertEquals("Number of connections handed out after get connection should be 1", 2, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after get connection should be 0", 0, connectionPool.getNumberOfConnectionsAvailable());
             
             String uuid3 = ((PooledConnectionInfo)connection3).getConnectionUuid();
             Assert.assertFalse("Should not be the same connection, got UUIDs: "+uuid1+", "+uuid3, uuid1.equals(uuid3));
             
-            connecionPool.releaseConnection(connection3);
-            Assert.assertEquals("Number of connections handed out after release connection should be 1", 1, connecionPool.getNumberOfConnectionsLeased());
-            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connecionPool.getNumberOfConnectionsAvailable());
+            connectionPool.releaseConnection(connection3);
+            Assert.assertEquals("Number of connections handed out after release connection should be 1", 1, connectionPool.getNumberOfConnectionsLeased());
+            Assert.assertEquals("Number of connections available after release connection should be 1", 1, connectionPool.getNumberOfConnectionsAvailable());
             
             Assert.assertFalse("Release connection should not be valid", ((PooledConnectionInfo)connection).isLeaseValid());
             Assert.assertTrue("not-yet-released connection should still be valid", ((PooledConnectionInfo)connection2).isLeaseValid());
